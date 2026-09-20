de33c304338d2d8cb26a36fee1e580e1285ab48d
Victor Thomas <victorathomas01@gmail.com>
2026-09-20 18:31:24 -0400

    Add sandwich_maker.py

---
+++ sandwich_maker.py
@@ -0,0 +1,17 @@
+class SandwichMaker:
+    def __init__(self, resources):
+        self.machine_resources = resources
+
+    def check_resources(self, ingredients):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                return False
+        return True
+
+    def make_sandwich(self, sandwich_size, order_ingredients):
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
